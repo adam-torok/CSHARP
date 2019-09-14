58c7b5e04f413b4c2225ea1056ce77b5be048a21
Adam Torok <44532426+woltery99@users.noreply.github.com>
2019-09-14 11:48:16 +0200

    penztargep

---
+++ Penztargep/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Penztargep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int osszeg = 0;
+            string input;
+
+            Console.WriteLine("Üdvözöljük a pénztár programban!");
+            Console.WriteLine("Kezdje el az árak bevitelét: ");
+            do
+            {
+                input = Console.ReadLine();
+                int resz; // kezdőértéke 0
+                // Ha nem sikerül, akkor változatlan a 'resz'
+                int.TryParse(input, out resz);
+                osszeg += resz;
+                // osszeg = osszeg + resz
+                // Ha üres akkor az input == ""
+                // vagy input != ""
+                Console.WriteLine("Eddigi összeg: " + osszeg);
+            } while (!string.IsNullOrEmpty(input));
+
+            Console.WriteLine("Végösszeg: " + osszeg);
+
+            Console.ReadKey();
+        }
+    }
+}
